4009afbd51d2f663f24e7e8d876001e36d4c0373
Austin Andrews <admin@templarian.com>
2022-08-22 20:18:16 -0500

    Test

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The Memory icon set contains 22x22 pixelated icons. Ideally for the Sharp Memory 2.7" Display.
 
-<img src="assets/preview.png" width="600">
+![Preview](assets/preview.svg)
 
 ## Contributions
 
--- scripts/generate-preview.js
+++ scripts/generate-preview.js
@@ -1,8 +1,44 @@
 // Generate preview.png
 
 const fs = require('fs');
+const path = require('path');
 
+const dir = fs.opendirSync('src')
+let file;
+const icons = [];
+while ((file = dir.readSync()) !== null) {
+    const svg = fs.readFileSync(path.join('src', file.name), 'utf-8');
+    const data = svg.match(' d="([^"]+)"')[1];
+    icons.push({
+        name: file.name.replace('.svg', ''),
+        path: data
+    });
+}
+dir.closeSync();
 
+const width = 600;
+const height = 300;
+const padding = 10;
+const border = 8;
+const bezel = 25 - border;
+const paths = icons.map((icon, i) => {
+    const columns = Math.floor((width - 50) / 28);
+    const x = i % columns;
+    const y = Math.floor(i / columns);
+    return `<g transform="translate(${padding + 25 + (x * 28)} ${padding + 25 + (y * 28)})"><path d="${icon.path}" fill="#322F28"/></g>`;
+});
+
+const svg = `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="${width}" height="${height}" fill="white"/>
+<rect width="${width}" height="${height}" rx="20" fill="#6437A3"/>
+<rect x="${border}" y="${border}" width="${width - (border * 2)}" height="${height - (border * 2)}" rx="16" fill="black"/>
+<rect x="25" y="25" width="${width - 50}" height="${height - 50}" fill="#B1AEA7"/>
+${paths.join("\n")}
+</svg>`;
+
+fs.writeFileSync(path.join('assets', 'preview.svg'), svg);
+
+/*
 fs.writeFileSync('preview.html', `<html>
 <head>
 
@@ -36,3 +72,4 @@ ctx.drawImage(getCanvas(path), 22, 22);
 </body>
 
 </html>`)
+*/
