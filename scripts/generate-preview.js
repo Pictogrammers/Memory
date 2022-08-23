84ad662fdc8fec1369221347721c9881da67beab
Austin Andrews <admin@templarian.com>
2022-08-22 20:28:29 -0500

    Fix preview.

--- scripts/generate-preview.js
+++ scripts/generate-preview.js
@@ -17,16 +17,17 @@ while ((file = dir.readSync()) !== null) {
 dir.closeSync();
 
 const width = 600;
-const height = 300;
 const padding = 10;
 const border = 8;
 const bezel = 25 - border;
+const columns = Math.floor((width - 50) / 28);
 const paths = icons.map((icon, i) => {
-    const columns = Math.floor((width - 50) / 28);
     const x = i % columns;
     const y = Math.floor(i / columns);
     return `<g transform="translate(${padding + 25 + (x * 28)} ${padding + 25 + (y * 28)})"><path d="${icon.path}" fill="#322F28"/></g>`;
 });
+
+const height = 48 + (Math.ceil(icons.length / columns) * 28) + (padding * 2);
 
 const svg = `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect width="${width}" height="${height}" fill="white"/>
