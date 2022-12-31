90a1a32de0f69527698bc7da238fc88cd7f95b27
Austin Andrews <admin@templarian.com>
2022-12-31 11:36:56 -0600

    v1.1 reorganize src

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 function getIcons() {
-    const dir = fs.opendirSync('src');
+    const svgDir = path.join('src', 'svg');
+    const dir = fs.opendirSync(svgDir);
     let file;
     const icons = [];
     while ((file = dir.readSync()) !== null) {
-        const svg = fs.readFileSync(path.join('src', file.name), 'utf-8');
+        const svg = fs.readFileSync(path.join(svgDir, file.name), 'utf-8');
         const data = svg.match(' d="([^"]+)"')[1];
         icons.push({
             name: file.name.replace('.svg', ''),
